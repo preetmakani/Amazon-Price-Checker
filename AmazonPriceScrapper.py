95adcf09f130bd4bfd73a1328ff54c8beb493373
Preet Makani <40505135+preetmakani@users.noreply.github.com>
2021-01-08 18:31:55 -0500

    user-agent

--- AmazonPriceScrapper.py
+++ AmazonPriceScrapper.py
@@ -7,7 +7,7 @@ import datetime
 URL = 'https://www.amazon.ca/Cancelling-Wireless-Bluetooth-Headphones-Soapstone/dp/B07X5F81JS/ref=asc_df_B07X5F81JS/?tag=googleshopc0c-20&linkCode=df0&hvadid=335360952415&hvpos=&hvnetw=g&hvrand=3988482828135226494&hvpone=&hvptwo=&hvqmt=&hvdev=c&hvdvcmdl=&hvlocint=&hvlocphy=1002347&hvtargid=pla-900062287677&psc=1'
 desiredPrice = 500 #change according to product
 
-headers = {"User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36'}
+headers = {"User-Agent": }
 
 
 
